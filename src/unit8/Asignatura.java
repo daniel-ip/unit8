005b818576d8dee1b3e3e1e1803e16afb22b846e
daniel-ip <daniel-ip86@hotmail.com>
2015-05-03 08:46:52 +0200

    Implementación de la clase Asignatura

--- src/unit8/Asignatura.java
+++ src/unit8/Asignatura.java
@@ -1,5 +1,33 @@
 package unit8;
 
+/***
+ * Asignaturas
+ * @author Daniel Íñigo Pérez
+ * @version 03/05/2015
+ */
 public class Asignatura {
 
+	private String nombre;
+	private double nota;
+	
+	public Asignatura(String nombre){
+		this.nombre = nombre;
+	}
+	
+	public String getNombre(){
+		return nombre;
+	}
+	
+	public void setNombre(String nombre){
+		this.nombre = nombre;
+	}
+	
+	
+	public double getNota(){
+		return nota;
+	}
+	
+	public void setNota(double nota){
+		this.nota = nota;
+	}
 }
